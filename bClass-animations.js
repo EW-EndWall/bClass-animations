095f9833bbcafa5bef442fde1fb4f1906bb6f688
EndWall <43109779+EW-EndWall@users.noreply.github.com>
2023-11-19 13:45:26 +0300

    update
    updated

--- bClass-animations.js
+++ bClass-animations.js
@@ -1,10 +1,17 @@
-$(".animation-items-m-1 >").hover(
-  function () {
-    $(this).addClass("animation-items-hovered-m-1");
-    $(".item").not(this).addClass("animation-items-not-hovered-m-1");
-  },
-  function () {
-    $(this).removeClass("animation-items-hovered-m-1");
-    $(".item").removeClass("animation-items-not-hovered-m-1");
-  }
-);
+/* * bClass animations v1.0.0 (--)
+ * * Copyright 2021-2023 The Bclass Authors
+ * * Licensed (--)
+ * * Update (--)
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  $(".animation-items-m-1 >").hover(
+    function () {
+      $(this).addClass("animation-items-hovered-m-1");
+      $(".item").not(this).addClass("animation-items-not-hovered-m-1");
+    },
+    function () {
+      $(this).removeClass("animation-items-hovered-m-1");
+      $(".item").removeClass("animation-items-not-hovered-m-1");
+    }
+  );
+});
